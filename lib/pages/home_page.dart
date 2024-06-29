7fed9ef27266d7fed564d443855173f3d7eb90cf
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-06-29 10:05:47 -0500

    buil botton agregar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,92 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                InkWell(
+                  onTap: () {
+                    print("Hola");
+                  },
+                  child: Container(
+                    color: Colors.black,
+                    height: 100,
+                    width: double.infinity,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      // crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          "Agregar",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 18,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(35),
+                        bottomRight: Radius.circular(35),
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Resumen de gastos",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Gestiona tus gastos de mejor forma",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black45,
+                          ),
+                        ),
+                        ListTile(
+                          title: Text("Compras en el super"),
+                          subtitle: Text("14/01/2025 23:21"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 75,
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
