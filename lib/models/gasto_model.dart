fe1871938484807600576c44e07710bdc66a5d41
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-06-30 10:27:03 -0500

    gasto model

---
+++ lib/models/gasto_model.dart
@@ -0,0 +1,13 @@
+class GastoModel {
+  String title;
+  double price;
+  String datetime;
+  String type;
+
+  GastoModel({
+    required this.title,
+    required this.price,
+    required this.datetime,
+    required this.type,
+  });
+}
