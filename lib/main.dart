d13129465bba3650cfc0fd4c91cfcd2efbfb8f6d
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-07-06 09:47:51 -0500

    intl

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:gastosappg8/pages/home_page.dart';
 
+// im;
 void main() {
   runApp(
     MaterialApp(
       home: HomePage(),
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        // GlobalMaterialLocalizations.
+      ],
+      supportedLocales: [
+        Locale('es', 'ES'),
+        Locale('en', 'US'),
+      ],
     ),
   );
 }
